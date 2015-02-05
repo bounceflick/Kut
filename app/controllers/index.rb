8b4fc98e6a95805edf5f6cbc7e8918d4ed4f0559
jayceef & maurei <pair+jayceef.maurei@devbootcamp.com>
2015-02-05 15:07:45 -0800

    Finished login and sign up page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,47 +1,66 @@
+get "/" do
+redirect '/login'
+end
+
+
+
 get '/login' do  #login page form
-  redirect '/user/#{session[:user_id]}' if session[:user_id]
-  <<-HTML
-  <h3>Login</h3>
-  <form action='/login' method='post'>
-    <input type='text' name='username'>
-    <input type='password' name='password'>
-    <input type='submit' value='submit'>
-  </form>
-  HTML
-  #erb :index
+  #redirect '/user/#{session[:user_id]}' if session[:user_id]
+  # <<-HTML
+  # <h3>Login</h3>
+  # <form action='/login' method='post'>
+  #   <input type='text' name='handle'>
+  #   <input type='password' name='password'>
+  #   <input type='submit' value='submit'>
+  # </form>
+  # HTML
+  erb :index
 end
 
 post '/login' do #peform login (post login data and decide what to do)
-  user = User.find_by(username: params[:username])
+  "landed"
+  user = User.find_by(handle: params[:handle])
   redirect '/login' unless user
-
+  "landed"
   if user.password == params[:password]
     session[:user_id] = user.id
-    redirect '/user/#{session[:user_id]}'
+    redirect "/user/#{session[:user_id]}"
   else
     redirect '/login'
   end
 end
 
 
+get '/user/new' do  # registration form
+  # redirect '/secret' if session[:user_id]
+
+  # <<-HTML
+  # <h3>Register as a User</h3>
+  # <form action='/user' method='post'>
+  #   <input type='text' name='user[handle]'>
+  #   <input type='password' name='user[password]'>
+  #   <input type='submit' value='submit'>
+  # </form>
+  # HTML
+end
+
+post '/user' do # create net user
+  "landed"
+  User.create(params[:user])
+  redirect '/login'
+end
+
 get '/user/:id' do #user timeline (landing page after login)
  <<-HTML
   <h3>Under Development</h3>
   <br><br>
   <p>Wooo this is my timeline</p>
+  <p> Hello user with id ##{params[:id]}!!
   HTML
 end
 
 
-get '/user/new' do  # registration form
-  User.create(params[:user])
-  redirect '/login'
-end
 
-post '/user' do # create net user
-  User.create(params[:user])
-  redirect '/login'
-end
 
 get '/logout' do
   session.delete :user_id
