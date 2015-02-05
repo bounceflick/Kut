f14534018acf51d3d4b077b18dc2d52fd9efa024
jayceef & maurei <pair+jayceef.maurei@devbootcamp.com>
2015-02-05 12:20:43 -0800

    Set up skeleton and database, about to test

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,6 @@
-require "SecureRandom"
 
-class Url < ActiveRecord::Base
-before_save :url_shortener, :http_prefixer
+class Tweet < ActiveRecord::Base
+belongs_to :user
 
-  def url_shortener
-    self.short_url = SecureRandom.urlsafe_base64(6)
-  end
-
-  def http_prefixer
-    self.url = "http://"+self.url unless self.url =~ %r{http://} or %r{https://}
-  end
 
 end
--- db/migrate/20150204112306_create_tweets_table.rb
+++ db/migrate/20150204112306_create_tweets_table.rb
@@ -1,8 +1,11 @@
 class CreateTweetsTable < ActiveRecord::Migration
   def change
-    create_table :urls do |t|
-      t.string  :url
-      t.string  :short_url
+    create_table :tweets do |t|
+
+      t.string :content
+      t.integer :user_id
+
+
 
       t.timestamps
     end
